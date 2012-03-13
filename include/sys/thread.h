070ac6097aa03911f69749d729959f662c8db03d
Michel Megens <dev@michelmegens.net>
2012-03-13 22:23:05 +0100

    thread: started with a remake of the thread submodule.

--- include/sys/thread.h
+++ include/sys/thread.h
@@ -21,71 +21,24 @@
 
 #include <inttypes.h>
 
-/*
- * max sched timer ticks before preempting the running thread
- */
-#define MAX_QUANTUM 5
+#ifndef RTSCHED
 
-typedef struct thread_t {
-  struct thread_t * runq_prev;    /* previous thread in run queue */
-  struct thread_t * runq_next;    /* next thread in run queue */
-  struct thread_t * sleep_q_next; /* next thread in sleep queue */
-  uint8_t         * sp;           /* thread stack pointer */
-  uint8_t         * stack;        /* start of thread stack */
-  uint8_t           priority;     /* thread priority */
-  uint8_t           ticks;        /* sched timer ticks remaining */
-  uint8_t           quantum;      /* sched timer ticks allowed */
-  uint8_t           flags;        /* thread flags - see AVRTH_* flags below */
-  uint16_t          stack_size;   /* thread stack size */
-  uint16_t          sleep_timer;  /* sleep timer */
-  void            * parm;         /* parameter pointer */
-} THREAD;
-
-#define AVRTH_VALID    0x01  /* thread is valid (in use) */
-#define AVRTH_RUNNABLE 0x02  /* thread is runnable */
-
-
-#define THREADS(n, istacksz) \
-  THREAD thread[n+2]; \
-  THREAD * main_thread; \
-  THREAD_DEF(idle, istacksz)
-
-#define THREAD_DEF(name, stacksz) \
-  uint8_t name##_stack[stacksz]; \
-  THREAD * name##_thread
-
-#define N_THREADS  (sizeof(thread)/sizeof(THREAD))
-
-/*
- * User code needs to declare these arrays, sized accordingly
- */
-extern THREAD  thread[];             /* max # threads allowed */
-extern uint8_t idle_stack[];         /* storage for idle thread stack */
-
-/*
- * these are declared within the threads code
- */
-extern THREAD * current_thread;
-extern uint8_t n_threads;
-extern uint8_t thread_preemption;
-
-
-void thread_tick(void);
-
-THREAD * threads_init(uint8_t max_threads, uint16_t mstacksz, uint16_t istacksz, 
-                      uint8_t main_prio, uint8_t preemption);
-
-void thread_yield(void);
-
-THREAD * thread_create(void (*entry)(void), uint8_t * stack, 
-                       uint16_t stack_size, uint8_t priority, void * parm);
-
-void thread_sleep(uint16_t ms);
-
-void thread_start(THREAD * t);
-
-void thread_stop(THREAD * t);
-
-void thread_kill(THREAD * t);
+/**
+  * \struct struct thread
+  * \brief Describes the state of a thread
+  */
+struct thread
+{
+        struct thread *next;
+        struct thread *prev;
+        void *stack;            /* start of the stack */
+        unsigned char *sp;      /* stack pointer */
+        void *param;            /* thread parameter */
+        unsigned char prio;
+        unsigned short stack_size;
+        unsigned int sleep_time;
+} __attribute__((packed));
+typedef struct thread THREAD;
+#endif
 
 #endif

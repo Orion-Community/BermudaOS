2f45708ae520b76217edada12861dbb93e6d4ab9
Michel Megens <dev@michelmegens.net>
2012-03-14 17:45:17 +0100

    timer: added timer files.

---
+++ include/arch/avr/328/timer.h
@@ -0,0 +1,31 @@
+/*
+ *  BermudaOS - Timers
+ *  Copyright (C) 2012   Michel Megens
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __TIMER328_H
+#define __TIMER328_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void BermudaInitTimer0();
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* __TIMER328_H */
---
+++ src/arch/avr/328/timer.c
@@ -0,0 +1,34 @@
+/*
+ *  BermudaOS - Timer handlers
+ *  Copyright (C) 2012   Michel Megens
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <arch/avr/io.h>
+
+void BermudaInitTimer0()
+{
+        cli();
+        /* fast pwm - TOP 0xff */
+        spb(TCCR0A, WGM00);
+        spb(TCCR0A, WGM01);
+
+        /* set prescaler */
+
+        /* re-enable interrupts */
+        sei();
+}

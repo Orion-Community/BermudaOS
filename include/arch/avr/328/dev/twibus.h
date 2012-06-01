fa10a14bd9af82c65f98d437beecaa59ea0ae8d4
Michel Megens <dev@michelmegens.net>
2012-06-01 19:10:56 +0200

    twi: added master transmitter status modes.

--- include/arch/avr/328/dev/twibus.h
+++ include/arch/avr/328/dev/twibus.h
@@ -16,6 +16,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+//! \file arch/avr/328/dev/twibus.h
+
 #ifndef __TWIF_H
 #define __TWIF_H
 
@@ -26,5 +28,45 @@
 #include <dev/twif.h>
 
 #include <sys/events/event.h>
+
+// Master transmitter status bytes
+
+/**
+ * \brief Start has been sent.
+ */
+#define TWI_MT_START_ACK 0x8
+
+/**
+ * \brief Repeated start has been sent.
+ */
+#define TWI_MT_RSTART_ACK 0x10
+
+/**
+ * \brief Slave address has been sent and ACKed.
+ */
+#define TWI_MT_SLA_ACK 0x18
+
+/**
+ * \brief Slave address has been sent and NACKed.
+ */
+#define TWI_MT_SLA_NACK 0x20
+
+/**
+ * \brief Data has been sent and ACKed.
+ */
+#define TWI_MT_DATA_ACK 0x28
+
+/**
+ * \brief Data has been sent and NACKed.
+ */
+#define TWI_MT_DATA_NACK 0x30
+
+/**
+ * \brief Bus arbitration has been lost.
+ * \warning Transmission has to be restarted or stopped!
+ * 
+ * The bus is lost in sending the SLA+W or by sending a data byte.
+ */
+#define TWI_MT_ARB_LOST 0x38
 
 #endif __TWIF_H

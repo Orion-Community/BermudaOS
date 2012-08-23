7143a62adeb178997ef2b78691f7ff0725f1830c
Michel Megens <dev@michelmegens.net>
2012-08-23 15:43:12 +0200

    stdio: added a specific file for the usart stream.

---
+++ src/arch/avr/328/serialio.c
@@ -0,0 +1,70 @@
+/*
+ *  BermudaOS - Serial I/O functions used by stdio
+ *  Copyright (C) 2012   Michel Megens
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// private functions
+
+PRIVATE WEAK int BermudaUsartReadByte(FILE *stream);
+PRIVATE WEAK int BermudaUsartWriteByte(char c, FILE *stream);
+
+#include <bermuda.h>
+#include <stdio.h>
+
+#include <arch/usart.h>
+#include <arch/avr/serialio.h>
+
+/**
+ * \brief Setup the USART file streams used by functions such as printf.
+ * \todo Put this function in serialio.c
+ */
+PUBLIC void BermudaUsartSetupStreams()
+{
+		fdev_setup_stream(&usart_out, &BermudaUsartWriteByte, NULL, _FDEV_SETUP_WRITE);
+		fdev_setup_stream(&usart_in, NULL, &BermudaUsartReadByte, _FDEV_SETUP_READ);
+		stdout = &usart_out;
+		stdin  = &usart_in;
+}
+
+/**
+ * \brief Writes a byte the serial bus.
+ * \todo Put this function in serialio.c
+ */
+PRIVATE WEAK int BermudaUsartWriteByte(char c, FILE *stream)
+{
+	HW_USART *hw = BermudaUsartGetIO(USART0);
+	
+	USART0->usartif->io(USART0, USART_STOP, NULL);
+	if(c == '\n') {
+		BermudaUsartWriteByte('\r', stream);
+	}
+
+	while(( (*(hw->ucsra)) & BIT(UDRE0) ) == 0);
+	(*(hw->udr)) = c;
+
+	return 0;
+}
+
+/**
+ * \brief Tries to read a byte from the serial bus.
+ * \todo Put this function in serialio.c
+ */
+PRIVATE WEAK int BermudaUsartReadByte(FILE *stream)
+{
+	unsigned char c = 0;
+	BermudaUsartTransfer(USART0, NULL, 0, &c, 1, 9600, 500);
+	return c;
+}

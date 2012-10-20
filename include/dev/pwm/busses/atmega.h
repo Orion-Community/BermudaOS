b6284e97948ccd5a5ecb2fbf6e957993ca1b6353
Michel Megens <dev@michelmegens.net>
2012-10-20 03:05:33 +0200

    pwm: added pwm source files.

---
+++ include/dev/pwm/busses/atmega.h
@@ -0,0 +1,28 @@
+/*
+ *  BermudaOS - ATmega AVR pwm header
+ *  Copyright (C) 2012   Michel Megens
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ATMEGA_AVR_PWM_H
+#define __ATMEGA_AVR_PWM_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <dev/dev.h>
+#include <dev/pwm/pwm.h>
+
+#endif /* __ATMEGA_AVR_PWM_H */
